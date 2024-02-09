70fa05b0934617bde4d9ea2b74ed9336acacb3e0
DiasMath <matheusdiassandes@outlook.com>
2024-02-09 20:07:36 -0300

    feat: Implementing Validator

--- src/errors/error_handle.py
+++ src/errors/error_handle.py
@@ -1,7 +1,18 @@
 from src.views.http_types.http_response import HttpResponse
-
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityErro
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityErro):
+        return HttpResponse(
+            status_code = error.status_code,
+            body = {
+                "errors": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
+
     return HttpResponse(
         status_code=500,
         body={
